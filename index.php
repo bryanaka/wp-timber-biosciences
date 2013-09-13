9ebee060a23a3bb60f1f15c2f19c7ceec335a323
Bryan M. Robles <bryanaka0@gmail.com>
2013-09-12 18:20:44 -0700

    trying to make things more OO and encapsulated...

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
 use Timber as Timber;
 use Biosciences as Biosciences;
 
-$context = Biosciences\Context::get_context();
-$context['posts'] = Timber::get_posts();
-$context['main_sidebar'] = Timber::get_widgets('main_sidebar');
-Timber::render('index.twig', $context);
+// how many posts you want to display on the home page
+$postCount = 5;
+
+$page = new Biosciences\Base();
+
+$menus = array('primary', 'footer_left', 'footer_center', 'footer_right');
+
+$page->add_menus($menus);
+$page->find_posts("numberposts={$postCount}");
+
+//$context['posts'] = Timber::get_posts("numberposts={$postCount}");
+
+$page->context['main_sidebar'] = Timber::get_widgets('main_sidebar');
+
+$page->render('index.twig');
--- lib/biosciences/base.php
+++ lib/biosciences/base.php
@@ -1,8 +1,59 @@
-<?php namespace Bioscences;
+<?php namespace Biosciences;
 
-Class Base {
+use Timber as Timber;
+use TimberMenu as TimberMenu;
 
-	public static function initialize () {
+Class Base extends Timber {
 
+	public $context;
+	public $posts;
+	public $menus = array();
+	public $sidebars = array();
+
+	public function __construct() {
+		parent::__construct();
+		$this->context = parent::get_context();
+	}
+
+	public function add_menu ( $menu_id ) {
+		$this->menus[$menu_id] = new TimberMenu($menu_id);
+	}
+
+	public function add_menus ( $menu_ids ) {
+		if( is_array($menu_ids) ) {
+			foreach ($menu_ids as $menu_id) {
+				$this->add_menu( $menu_id );
+			}
+			return true;
+		} else {
+			return false; 
+		}
+	}
+
+	public function find_posts($query = false, $PostClass = 'TimberPost', $replace_posts = false) {
+		$results = parent::get_posts($query, $PostClass);
+		if( !isset($this->posts) || $replace_posts) {
+			$this->posts = $results;
+		}
+		return $results;
+	}
+	// how could we filter posts from these results if needed? how to reduce queries?
+
+	public function render_page( $filenames, $data = Array(), $echo = true ) {
+		$this->add_to_context('menus');
+		if ( !empty($data) ) {
+			$data = $this->context;
+		}
+		parent::render($filenames, $data, $echo);
+
+	}
+
+	private function add_to_context ($property) {
+		if( property_exists($this, $property) ) {
+			// add a check to see if this property is valid
+			$this->context[$property] = $this->{$property};
+		} else {
+			throw new InvalidArgumentException("No Property Found with the name {$property} in " +get_class($this));
+		}
 	}
 }
