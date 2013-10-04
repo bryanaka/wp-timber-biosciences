88c7926924c32d8601d306aeb7ab248945c14a21
Bryan M. Robles <bryanaka0@gmail.com>
2013-10-04 13:37:03 -0700

    made some alterations to the main page

--- index.php
+++ index.php
@@ -12,4 +12,5 @@ $page->context['main_sidebar'] = Timber::get_widgets('main_sidebar');
 // this is akin to an actual controller action...
 $postCount = 5;
 $page->find_posts("numberposts={$postCount}");
+// need to find the first 5 featured posts as well. Can we do this in one query?
 $page->render_page('index.twig');
