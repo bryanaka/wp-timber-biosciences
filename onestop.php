d7bd85d3fbaf27e12db54ccde9d8169979a2244c
Jerry Chung <jerrychung@lbl.gov>
2013-11-12 16:33:11 -0800

    More onestop work

--- onestop.php
+++ onestop.php
@@ -7,6 +7,11 @@ use Timber as Timber;
 use TimberMenu as TimberMenu;
 use TimberPost as TimberPost;
 
+$query = array('post_type' => 'page',
+    'post_parent__in' => array(71),
+    'posts_per_page' => -1
+    );
+
 $context = Timber::get_context();
-$context['post'] = new TimberPost(190);
+$context['onestop_posts'] = Timber::get_posts($query);
 Timber::render('views/onestop/index.twig', $context);
