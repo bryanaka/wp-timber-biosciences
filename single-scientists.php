ea9cdd98fceb6b86eaf44605c93e46f716ae2c29
Crystal Lee <cris4allee@berkeley.edu>
2014-03-07 14:23:06 -0800

    pdf upload path fixed

--- single-scientists.php
+++ single-scientists.php
@@ -6,7 +6,10 @@ use Biosciences as Biosciences;
 $page = new Biosciences\Base();
 $menus = array('primary', 'footer_left', 'footer_center', 'footer_right');
 $page->find_menus($menus);
-
 $page->get_current_post('scientist');
+
+$post_scientist = new TimberPost();
+$file_path = wp_get_attachment_url($post_scientist->publications);
+$page->context['file_path'] = $file_path;
 
 $page->render_page('scientists/single.twig');
